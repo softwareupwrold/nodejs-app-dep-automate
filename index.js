4d2b06f58cff1b998615034f2bc64b375ebf77f6
softwareupwrold <upwroldsoftware@gmail.com>
2026-02-28 09:48:32 +0545

    ch_1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get ('/', (req, res)  => {
-    return res.json({ msg: "wow1 Hello from the server 123"})
+    return res.json({ msg: " Hello from the server 123"})
 })
 
 app.listen(PORT, () => {
